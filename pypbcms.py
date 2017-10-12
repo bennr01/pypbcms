15c592a24e95dd6f6714decae672e9c42153c006
bennr01 <benjamin99.vogt@web.de>
2017-10-12 23:02:12 +0200

    add some initial commands

--- pypbcms.py
+++ pypbcms.py
@@ -8,8 +8,17 @@ import shutil
 import shlex
 import argparse
 import sys
+import platform
+import socket
+import pprint
+import time
 
-from twisted.internet import task, threads, protocol, defer, endpoints, reactor
+try:
+    import multiprocessing
+except ImportError:
+    multiprocessing = None
+
+from twisted.internet import task, threads, protocol, defer, endpoints, utils, reactor
 from twisted.internet.error import ProcessDone, ConnectionDone
 from twisted.python.log import startLogging
 from twisted.python.util import println
@@ -47,6 +56,16 @@ class ProcessControlProtocol(protocol.ProcessProtocol):
             self.d.callback(0)
         else:
             self.d.callback(v.exitCode)
+
+
+def is_None(obj):
+    """returns True if object is None."""
+    return (obj is None)
+
+
+def is_not_None(obj):
+    """returns True if object is not None."""
+    return (obj is not None)
 
 
 # ================= PROTOCOLS ======================
@@ -124,9 +143,10 @@ class ServerAndClientSharedProtocol(JsonProtocol):
             n = msg.get("action_name", None)
             if (n is None) or ("." in n):
                 return
-            if not hasattr(self, n):
+            fn = "handle_" + n
+            if not hasattr(self, fn):
                 return
-            f = getattr(self, "handle_" + n)
+            f = getattr(self, fn)
             a = msg.get("data", None)
             return f(a)
         else:
@@ -166,9 +186,12 @@ class ServerAndClientSharedProtocol(JsonProtocol):
 
 class ServerProtocol(ServerAndClientSharedProtocol):
     """The protocol for the server"""
-    def __init__(self, factory):
+    def __init__(self, factory, cid):
         ServerAndClientSharedProtocol.__init__(self)
         self.factory = factory
+        self.info = {}
+        self.cid = cid
+        self.working = False
 
     def connectionMade(self):
         """called when the connection was made."""
@@ -184,6 +207,20 @@ class ServerProtocol(ServerAndClientSharedProtocol):
         self.send_version()
         if v != VERSION:
             reactor.callLater(1.0, self.disconnect)
+
+    def handle_set_info(self, info):
+        """sets the local system info to the received system info."""
+        self.info = info
+
+    def handle_set_ready(self, msg):
+        """sets the ready state."""
+        ns = msg.get("working", False)
+        self.working = ns
+        self.info["working"] = self.working
+
+    def run_command(self, command):
+        """runs the command on the client."""
+        self.call_action("shell", {"command": command})
 
 
 class ClientProtocol(ServerAndClientSharedProtocol):
@@ -210,6 +247,46 @@ class ClientProtocol(ServerAndClientSharedProtocol):
         if v != VERSION:
             println("Error: Version mismatch! Client version: {c} Server version: {s}.".format(c=VERSION, s=v))
             self.disconnect()
+        else:
+            self.send_info()
+            self.set_working(False)
+
+    @defer.inlineCallbacks
+    def handle_shell(self, msg):
+        """handles a shell command."""
+        command = msg.get("command", None)
+        if command is None:
+            pass
+        else:
+            print command
+            self.set_working(False)
+            v = yield utils.getProcessOutput(command[0], command)
+            print "value: ", v
+            self.set_working(True)
+
+    def handle_sendinfo(self, msg):
+        """sends our system information *again*."""
+        self.send_info()
+        self.set_working()
+
+    def send_info(self):
+        """sends platform information to the server."""
+        data = {
+            "hostname": socket.gethostname(),
+            "machine": platform.machine(),
+            "node": platform.node(),
+            "platform": platform.platform(),
+            "processor": platform.processor(),
+            "python_implementation": platform.python_implementation(),
+            "system": platform.system(),
+            "release": platform.release(),
+            "pid": os.getpid(),
+            }
+        self.call_action("set_info", data)
+
+    def set_working(self, state):
+        """tells the server that this client is (not) working."""
+        self.call_action("set_working", {"working": state})
 
 
 class MessageReceiverProtocol(protocol.Protocol):
@@ -260,21 +337,80 @@ class ServerFactory(protocol.Factory):
 
     def __init__(self):
         self.clients = {}
+        self.cur_cid = 0
 
     def buildProtocol(self, addr):
         """builds a protocol for the communication with the client and returns it."""
-        p = ServerProtocol(self)
+        p = ServerProtocol(self, cid=self.cur_cid)
+        self.cur_cid += 1
         return p
 
     def add_client(self, p):
         """adds a client to the internal client list."""
-        peer = p.transport.getPeer()
+        peer = p.cid
         self.clients[peer] = p
 
     def remove_client(self, p):
         """removes a client from the internal client list."""
-        peer = p.transport.getPeer()
+        peer = p.cid
         del self.clients[peer]
+
+    def list_client_ids(self):
+        """returns a list containting the client ids of all currently connected clients."""
+        return self.clients.keys()
+
+    def get_client(self, cid):
+        """returns the client with the given client id or None if it is not found."""
+        return self.clients.get(cid, None)
+
+    def get_clients(self, cids, include_None=True):
+        """returns a list containing the clients with the given client ids."""
+        ret = []
+        for c in cids:
+            p = self.get_client(c)
+            if (p is None) and (not include_None):
+                continue
+            ret.append(p)
+        return ret
+
+    def get_working_client_ids(self):
+        """returns a list containing the client ids of all the working clients."""
+        t = filter(is_not_None, [e if self.get_client(e).working else None for e in self.list_client_ids()])
+        return t
+
+    def search_for_clients(self, conditions, include_empty=False):
+        """
+        Returns a list of all client ids of the clients matching the conditions.
+        'conditions' should be a dictionar specifying the key: values to check for matches in the client information.
+        A client is considered matching the conditions when all values of the keys defined in 'conditions' match with
+        the values of the same keys in the client information.
+        If 'include_empty' (default: False), empty client information is considered a match.
+        """
+        matches = []
+        cids = self.list_client_ids()
+        for cid in cids:
+            p = self.get_client(cid)
+            if p is None:
+                continue
+            if p.info == {}:
+                if (p.info == conditions) or include_empty:
+                    matches.append(cid)
+                else:
+                    continue
+            else:
+                dm = True
+                for k in conditions:
+                    if not k in p.info:
+                        dm = False
+                        break
+                    v1 = conditions[k]
+                    v2 = p.info[k]
+                    if v1 != v2:
+                        dm = False
+                        break
+                if dm:
+                    matches.append(cid)
+        return matches
 
 
 
@@ -282,25 +418,63 @@ class ServerFactory(protocol.Factory):
 
 class ManagementShell(cmd.Cmd):
     """The management shell."""
-    prompt = "({n})".format(n=NAME)
     intro = "{n} v{v}".format(n=NAME, v=VERSION)
 
     def __init__(self, factory):
         cmd.Cmd.__init__(self)
+        # self.prompt = "({n})".format(n=NAME)
         self.factory = factory
+        self.selected = []
+        self.update_prompt()
 
     def write(self, msg):
         """writes a message."""
         self.stdout.write(msg)
 
+    def pprint(self, obj):
+        """pretty prints object."""
+        s = pprint.pformat(obj)
+        if not s.endswith("\n"):
+            s += "\n"
+        self.write(s)
+
+    def filter_selected(self):
+        """removes all invalid client selections from the selection, including disconnected clients."""
+        # 1. remove all invalid (non-int) values
+        self.selected = filter(is_not_None, [e if isinstance(e, (int, long)) else None for e in self.selected])
+        # 2. remove all disconnected/non-existent clients
+        self.selected = filter(is_not_None, [e if (self.factory.get_client(e) is not None) else None for e in self.selected])
+
+    def update_prompt(self):
+        """updates the prompt."""
+        n_selected = len(self.selected)
+        n_working = len(self.factory.get_working_client_ids())
+        if n_working > 0:
+            ws = "|{n} working".format(n=n_working)
+        else:
+            ws = ""
+        self.prompt = "({n} selected{ws})".format(n=n_selected, ws=ws)
+
+    def precmd(self, line):
+        """called before a command executes."""
+        self.filter_selected()
+        self.update_prompt()
+        return line
+
+    def postcmd(self, stop, line):
+        """called when a command finished."""
+        self.filter_selected()
+        self.update_prompt()
+        return stop
+
     def do_EOF(self, l):
-        """exits the script."""
+        """EOF|quit|exit|q: exits the shell and stops the server."""
         return True
 
     do_exit = do_quit = do_q = do_EOF
 
     def do_shell(self, l):
-        """processes a shell command."""
+        """shell <cmd>: executes cmd in a local shell."""
         r = threads.blockingCallFromThread(reactor, self._do_shell, l)
         self.write("Done. Exit code: {r}.\n".format(r=r))
 
@@ -317,6 +491,83 @@ class ManagementShell(cmd.Cmd):
             childFDs={0:0, 1:1, 2:2},
             )
         return d
+
+    def do_select(self, l):
+        """select <ALL|NONE|condition> [-e]: sets the selected clients."""
+        args = shlex.split(l)
+        include_empty = False
+        if len(args) == 1:
+            cs = args[0]
+            if cs == "ALL":
+                self.selected = self.factory.list_client_ids()
+                self.update_prompt()
+                return
+            elif cs == "NONE":
+                self.selected = []
+                self.update_prompt()
+                return
+        elif len(args) == 2:
+            if "-e" not in args:
+                self.write("Usage Error: unknown argument.\n")
+                return
+            elif "-e" in args:
+                include_empty = True
+                args.remove("-e")
+                cs = args[0]
+            else:
+                raise RuntimeError("Logic Error!")
+        else:
+            self.write("Usage error. Usage: select [-e] <condition|ALL|NONE>\n")
+        try:
+            c = eval(cs)
+        except Exception as e:
+            self.write("Error evaluating selection conditions:\n")
+            self.write(repr(e) + "\n")
+            return
+        if isinstance(c, (int, long)):
+            self.selected = [c]
+        elif isinstance(c, (list, tuple)):
+            self.selected = []
+            for e in c:
+                if not isinstance(e, (int, long)):
+                    self.write("WARNING: invalid list/tuple element '{e}' ignored!\n".format(e=e))
+                else:
+                    self.selected.append(e)
+            self.selected = c
+            self.filter_selected()
+            self.update_prompt()
+        self.selected = self.factory.search_for_clients(c, include_empty=include_empty)
+        self.update_prompt()
+
+    def do_selected(self, l):
+        """selected: prints the selected ids."""
+        self.pprint(self.selected)
+
+    def do_show(self, l):
+        """show <cid>: shows the client info."""
+        try:
+            c = int(l)
+        except ValueError:
+            self.write("Error: Invalid argument!\n")
+            return
+        p = self.factory.get_client(c)
+        if c is None:
+            self.write("Error: No such client!\n")
+        self.pprint(p.info)
+
+    def do_disconnect(self, l):
+        """disconnect: disconnects all selected clients."""
+        for c in self.selected:
+            p = self.factory.get_client(c)
+            p.disconnect()
+        time.sleep(0.5)
+
+    def do_remotecommand(self, l):
+        """remotecommand [cmd]: executes cmd on the remote servers."""
+        rc = shlex.split(l)
+        clients = self.factory.get_clients(self.selected, include_None=False)
+        for c in clients:
+            c.run_command(rc)
 
 
 
